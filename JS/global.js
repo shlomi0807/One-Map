16aa90c5c61952cc1daa80b68f22004baa15f5bf
shlomi0807 <shlomi0807@gmail.com>
2026-06-09 18:50:39 +0300

    added SS to search + change islands style + delete form animation

--- JS/global.js
+++ JS/global.js
@@ -1,26 +1,12 @@
-// func to open the sidenav using JQUERY
-    $('#open-nav-btn').on('click', function() {
-    $('#mySidenav').css('width', '250px');
-    $(this).fadeOut(200);
-     $('#page-overlay').fadeIn(200);
-    });
-
-    // func to close the sidenav using JQUERY
-    $('#close-nav-btn, #page-overlay').on('click', function() {
-    $('#mySidenav').css('width', '0');
-    $('#open-nav-btn').fadeIn(200)
-    $('#page-overlay').fadeOut(200);
-    });
-
-
-
 
 // search results and filtering behvior (using JQUERY)
 function initSearch(dataToSearch, onItemSelect) {
     $('#search-input').on('input', function () {
 
-        // get what the user type and convert to lowercase
-        const inputVal = $(this).val().toLowerCase();
+        const inputVal = $(this).val(); // שומר את הערך המקורי
+        sessionStorage.setItem('lastSearch', inputVal); // שמירה ב-SessionStorage
+        
+        const inputValLower = inputVal.toLowerCase();  // get what the user type and convert to lowercase
         const resultsContainer = $('#search-results');
         const searchBar = $('#search');
         resultsContainer.empty(); // clear previous results
@@ -34,7 +20,7 @@ function initSearch(dataToSearch, onItemSelect) {
 
         // filter allMapData depending on what the user typed
         const filteredData = dataToSearch.filter(item =>
-            item.name.toLowerCase().includes(inputVal)
+            item.name.toLowerCase().includes(inputValLower)
         );
 
         // if no matches found, show a gray message
@@ -45,11 +31,11 @@ function initSearch(dataToSearch, onItemSelect) {
             filteredData.forEach(item => {
                 const resultItem = $('<a></a>').text(item.name);
                 
-
                 resultItem.on('click', function (e) {
                     e.preventDefault(); // Prevent default link behavior
 
                     $('#search-input').val(item.name); // fill the input with the selected name
+                    sessionStorage.setItem('lastSearch', item.name); // update SS to the selected name
                     resultsContainer.hide(); // hide the dropdown
                     searchBar.removeClass('active');
                     
@@ -65,8 +51,47 @@ function initSearch(dataToSearch, onItemSelect) {
     });
 }
 
-// func of x button behvior (using JQUERY)
-$(document).ready(function() {
+$(document).ready(function() { // wait to finish build the document 
+
+    const savedSearch = sessionStorage.getItem('lastSearch'); // var to save search item in ss
+    
+    // check if there was data saved at ss
+    if (savedSearch) {
+        const searchInput = document.getElementById('search-input');
+        // check if search input exists
+        if (searchInput) {
+            searchInput.value = savedSearch;
+        }
+    }
+
+    // func to open the sidenav using JQUERY
+    $('#open-nav-btn').on('click', function() {
+        $('#mySidenav').css('width', '250px');
+        $(this).fadeOut(200);
+        $('#page-overlay').fadeIn(200);
+    });
+
+    // func to close the sidenav using JQUERY
+    $('#close-nav-btn, #page-overlay').on('click', function() {
+        $('#mySidenav').css('width', '0');
+        $('#open-nav-btn').fadeIn(200)
+        $('#page-overlay').fadeOut(200);
+    });
+
+    // func to hide search results and search area focus (using JQUERY)
+    $(document).on('click', function(event) {
+        // check if the click was outside the search area
+        if (!$(event.target).closest('#search').length) {
+            
+            $('#search-results').hide(); // hides results
+            $('#search').removeClass('active'); // removes style if any
+            $('#search-input').blur(); // removing the curser
+        }
+    });
+
+    
+    // x button behvior (using JQUERY)   
+    
     const $searchInput = $('#search-input');
     const $clearBtn = $('#clear-btn');
 
@@ -83,21 +108,12 @@ $(document).ready(function() {
     $clearBtn.on('click', function(e) {
         e.preventDefault();
         $searchInput.val(''); // clears search area
+        sessionStorage.removeItem('lastSearch'); // delete from ss
         
         // re-trigger the typing event to returns the rest of the items back to the screen
         $searchInput.trigger('input'); 
-        
+
         $searchInput.focus();
     });
-});
 
-// func to hide search results and search area focus (using JQUERY)
-$(document).on('click', function(event) {
-    // check if the click was outside the search area
-    if (!$(event.target).closest('#search').length) {
-        
-        $('#search-results').hide(); // hides results
-        $('#search').removeClass('active'); // removes style if any
-        $('#search-input').blur(); // removing the curser
-    }
 });
